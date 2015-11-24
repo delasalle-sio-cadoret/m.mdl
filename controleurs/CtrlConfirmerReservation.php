f2729d0768e685a88fe26be28c2eccb21ee53ddf
delasalle-sio-cadoret <cadoretyoun@gmail.com>
2015-11-24 15:11:51 +0100

    test

---
+++ controleurs/CtrlConfirmerReservation.php
@@ -0,0 +1,86 @@
+<?php
+// Projet Réservations M2L - version web mobile
+// Fonction du contrôleur CtrlConsulterReservationsConnecter.php : Consulter les reservations d'un utilisateir
+// Ecrit le 03/11/2015 par Jim
+// Ce contrôleur vérifie l'authentification de l'utilisateur
+// si l'authentification est bonne, il affiche le menu utilisateur ou administrateur (vue VueMenu.php)
+// si l'authentification est incorrecte, il réaffiche la page de connexion (vue VueConnecter.php)
+// on teste si le terminal client est sous Android, pour lui proposer de télécharger l'application Android
+// tests des variables de session
+if ( ! isset ($_SESSION['nom']) == true)  $nom = '';  else  $nom = $_SESSION['nom'];
+if ( ! isset ($_SESSION['mdp']) == true)  $mdp = '';  else  $mdp = $_SESSION['mdp'];
+if ( ! isset ($_SESSION['niveauUtilisateur']) == true)  $niveauUtilisateur = '';  else  $niveauUtilisateur = $_SESSION['niveauUtilisateur'];
+// si l'utilisateur n'est pas encore identifié, il sera automatiquement redirigé vers le contrôleur d'authentification
+// (sauf s'il ne peut pas se connecter et demande de se faire envoyer son mot de passe qu'il a oublié)
+if ($nom == '' && $action != 'DemanderMdp') header ('Location: index.php?action=Connecter');
+// connexion du serveur web à la base MySQL
+include_once ('modele/DAO.class.php');
+$dao = new DAO();
+if ( empty ($_POST ["numReservation"]) == true)  $numReservation = ""; else $numReservation = $_POST["numReservation"];
+	
+if ($numReservation == '') {
+	// si les données sont incomplètes, réaffichage de la vue avec un message explicatif
+	$msgFooter = 'Données incomplètes';
+	$themeFooter = $themeProbleme;
+	include_once ('vues/VueConfirmerReservation.php');
+}
+if ( ! $dao->existeReservation($numReservation) )
+{	
+	$msgFooter = "Erreur : numéro de réservation inexistant";
+	$themeFooter = $themeProbleme;
+	include_once ('vues/VueConfirmerReservation.php');
+}
+else {
+	if ( ! $dao->estLeCreateur($nom, $numReservation) )
+	{	
+		$msgFooter = "Erreur : vous n'êtes pas l'auteur de cette réservation";
+		$themeFooter = $themeProbleme;
+		include_once ('vues/VueConfirmerReservation.php');
+	}
+	else {
+		$dao->creerLesDigicodesManquants();
+		if ( $dao->getReservation($numReservation)->getStatus() == 0 )
+		{
+			$msgFooter = "Erreur : cette réservation est déjà confirmée";
+			$themeFooter = $themeProbleme;
+			include_once ('vues/VueConfirmerReservation.php');
+		}
+		else {
+			if ( $dao->getReservation($numReservation)->getStart_time() < time() )
+			{
+				$msgFooter = "Erreur : cette réservation est déjà passée";
+				$themeFooter = $themeProbleme;
+				include_once ('vues/VueConfirmerReservation.php');
+			}
+			else {
+				$dao->confirmerReservation($numReservation);
+				
+				// recherche de l'adresse mail
+				$adrMail = $dao->getUtilisateur($nom)->getEmail();
+				// recherche du digicode
+				$digicode = $dao->getReservation($numReservation)->getDigicode();
+				
+				// envoie un mail de confirmation de l'enregistrement
+				$sujet = "Confirmation de réservation";
+				$message = "Nous avons bien enregistré la confirmation de la réservation N° " . $numReservation . "\n\n";
+				$message .= "Le digicode d'accès à la salle est : " . $digicode . "\n";
+				$message .= "Il est valable 1 heure avant la réservation, et pendant 1 heure après la réservation.";
+				$ok = Outils::envoyerMail($adrMail, $sujet, $message, $ADR_MAIL_EMETTEUR);
+				
+				if ( $ok )
+				{
+					$msgFooter = "Enregistrement effectué ; vous allez recevoir un mail de confirmation";
+					$themeFooter = $themeNormal;
+					include_once ('vues/VueConfirmerReservation.php');
+				}
+				else
+				{
+					$msgFooter = "Enregistrement effectué ; l'envoi du mail de confirmation a rencontré un problème";
+					$themeFooter = $themeNormal;
+					include_once ('vues/VueConfirmerReservation.php');
+				}
+			}
+		}
+	}
+}
+?>
---
+++ vues/VueCreerUtilisateur.php
@@ -0,0 +1,50 @@
+<?php
+	// Projet Réservations M2L - version web mobile
+	// Fonction de la vue VueDemanderMdp.php : visualiser la vue de demande d'envoi d'un nouveau mot de passe
+	// Ecrit le 12/10/2015 par Jim
+?>
+<!doctype html>
+<html>
+	<head>
+		<?php include_once ('vues/head.php'); ?>
+	</head> 
+	<body>
+		<div data-role="page">
+			<div data-role="header" data-theme="<?php echo $themeNormal; ?>">
+				<h4>M2L-GRR</h4>
+				<a href="index.php?action=Menu">Retour Menu</a>
+			</div>
+			<div data-role="content">
+				<h4 style="text-align: center; margin-top: 10px; margin-bottom: 10px;">Créer un Compte utillisateur</h4>
+				<form name="form1" id="form1" action="index.php?action=CreerUtilisateur" method="post">
+					<div data-role="fieldcontain" class="ui-hide-label">
+						<input type="text" name="nom" id="nom" placeholder="Entrez le nom de l'utilisateur"  >
+					</div>
+					<div data-role="fieldcontain" class="ui-hide-label">
+						<input type="email" name="mail" id="mail" placeholder="Entrez l'adresse mail"  >
+					</div>
+					<h4>Niveau :</h4>
+					
+					<div data-role="fieldcontain" class="ui-hide-label">
+					        <label><input name="niveau" type="radio" value="1">Invité</label>
+                            <label><input name="niveau" type="radio" value="2">Utilisateur</label>
+                            <label><input name="niveau" type="radio" value="3">Administrateur</label>
+					</div>
+					<div data-role="fieldcontain" class="ui-hide-label">
+						<input type="submit" name="creerUtilisateur" id="creerUtilisateur" value="Créer l'utilisateur">
+					</div>
+				</form>
+				
+				<?php if($debug == true) {
+					// en mise au point, on peut afficher certaines variables dans la page
+					echo "<p>SESSION['nom'] = " . $_SESSION['nom'] . "</p>";
+					echo "<p>SESSION['mdp'] = " . $_SESSION['mdp'] . "</p>";
+					echo "<p>SESSION['niveauUtilisateur'] = " . $_SESSION['niveauUtilisateur'] . "</p>";
+				} ?>
+			</div>
+			<div data-role="footer" data-position="fixed" data-theme="<?php echo $themeFooter; ?>">
+				<h4><?php echo $msgFooter; ?></h4>
+			</div>
+		</div>
+	</body>
+</html>
